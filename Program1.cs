bc64067359e86a193f6dfeaf0eab4f09ad22136b
Treiks <100555676+Treiks@users.noreply.github.com>
2022-04-04 10:54:15 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Test
+{
+    public static void Main()
+    {
+        string path = @"c:\temp\MyTest.txt";
+        if (!File.Exists(path))
+        {
+          
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine("Hello");
+                sw.WriteLine("And");
+                sw.WriteLine("Welcome");
+            }
+        }
+
+       
+        using (StreamReader sr = File.OpenText(path))
+        {
+            string s;
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
